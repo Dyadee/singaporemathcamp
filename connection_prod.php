02b305dc0e1acdf1e6854db73ced9c7e35f511a3
jade <jade@gmail.com>
2020-11-01 18:55:01 +0800

    used another mysql credentials

--- connection_prod.php
+++ connection_prod.php
@@ -1,3 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', '33c3d15feaeec47ce5978c2194639bd944ec9fd403bdefca', 'singaporemathcamp') or die(mysql_error($mysqli));
+require_once("error.php");
+
+$mysqli = new mysqli('localhost', 'barehacks', 'samplepassword', 'singaporemathcamp') or die(mysql_error($mysqli));
 ?>
